efa0810e29b51e290ca4943aaee96419639c216f
Xoma-boy <homahomic9@gmail.com>
2026-07-23 10:01:11 +0200

    Update cache name from v5 to v7

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finance-pro-v5';
+const CACHE_NAME = 'finance-pro-v7';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
